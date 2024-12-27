f913b2a3eeb4068fd8f94f54272270c291f58394
roshanjha490 <kumarroshanjha786@gmail.com>
2024-12-27 08:15:42 +0530

    completed registration part

--- index.js
+++ index.js
@@ -1,15 +1,122 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'form.html'));
 })
+
+app.get('/registrations', (req, res) => {
+    
+})
+
+app.get('/countries', (req, res) => {
+    const dataPath = path.join(__dirname, '/storage/countriesData.json');
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            res.status(500).json({ error: "Failed to fetch countries data" });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+})
+
+
+app.post("/registration", (req, res) => {
+    const registrationData = req.body;
+
+    const errors = [];
+
+    const dataPath = path.join(__dirname, "/storage/countriesData.json");
+    let countriesData;
+    try {
+        const data = fs.readFileSync(dataPath, "utf8");
+        countriesData = JSON.parse(data);
+    } catch (error) {
+        console.error("Error reading countries data:", errors);
+        return res.status(500).json({ status: false, error: ["Server Side Error Occured"] });
+    }
+
+    if (!registrationData.first_name || registrationData.first_name.trim() === "") {
+        errors.push("First name is required.");
+    }
+
+    if (!registrationData.last_name || registrationData.last_name.trim() === "") {
+        errors.push("Last name is required.");
+    }
+
+    if (!registrationData.email || registrationData.email.trim() === "") {
+        errors.push("Email is required.");
+    } else {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(registrationData.email.trim())) {
+            errors.push("Invalid email format.");
+        }
+    }
+
+    if ((registrationData.gender != 'Male') && (registrationData.gender != 'Female')) {
+        errors.push("Gender must be 'Male' or 'Female'.");
+    }
+
+    if (!registrationData.dob || registrationData.dob.trim() === "") {
+        errors.push("Date of birth is required.");
+    } else {
+        const today = new Date();
+        const dob = new Date(registrationData.dob);
+        const age = today.getFullYear() - dob.getFullYear() -
+            (today < new Date(today.getFullYear(), dob.getMonth(), dob.getDate()) ? 1 : 0);
+        if (age < 14 || age > 99) {
+            errors.push("Age must be between 14 and 99.");
+        }
+    }
+
+    const findCountry = countriesData.find((country) => country.name === registrationData.country);
+    if (!findCountry) {
+        errors.push("Invalid country selected.");
+    }
+
+    const findState = findCountry?.states.find((state) => state.name === registrationData.state);
+    if (!findState) {
+        errors.push("Invalid state selected.");
+    }
+
+    if (!findState?.cities.includes(registrationData.city)) {
+        errors.push("Invalid city selected.");
+    }
+
+    // Return errors if any
+    if (errors.length > 0) {
+        return res.status(400).json({ status: false, errors: errors });
+    }
+
+    const filePath = path.join(__dirname, "/storage/registrations.json");
+    fs.readFile(filePath, "utf8", (err, data) => {
+        let registrations = [];
+        if (!err && data) {
+            registrations = JSON.parse(data);
+        }
+        registrations.push(registrationData);
+
+        fs.writeFile(filePath, JSON.stringify(registrations, null, 2), (err) => {
+            if (err) {
+                console.error("Error saving registration:", err);
+                return res.status(500).json({ status: false, error: ["Failed to save registration data"] });
+            }
+            res.status(200).json({status: true, message: "Registration saved successfully" });
+        });
+    });
+
+});
+
+
 
 
 app.listen(port, () => {
